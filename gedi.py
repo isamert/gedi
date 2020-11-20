49100876041a6d6ecca4a8534e03209834357089
Mubashshir <ahmubashshir@gmail.com>
2020-11-20 09:31:28 +0600

    hashbang and icon name support

--- gedi.py
+++ gedi.py
@@ -7,17 +7,23 @@ from jedi.api import Script
 from gi.repository import GObject, Gedit, Gtk, GtkSource, GLib
 from threading import Thread, Event
 
-#FIXME: find real icon names
-icon_names = {'import': '',
-              'module': '',
-              'class': '',
-              'function': '',
-              'statement': '',
-              'param': ''}
+# FIXME: find real icon names
+icon_names = {
+    'module':   'package-x-generic',
+    'class':    'folder',
+    'instance': 'edit-copy',
+    'function': 'insert-object',
+    'param':    'insert-link',
+    'keyword':  'format-text-bold',
+    'property': 'find-location',
+    'statement': 'view-continuous',
+}
+
 
 class Jedi:
     def get_script(document):
-        doc_text = document.get_text(document.get_start_iter(), document.get_end_iter(), False)
+        doc_text = document.get_text(
+            document.get_start_iter(), document.get_end_iter(), False)
         iter_cursor = document.get_iter_at_mark(document.get_insert())
         linenum = iter_cursor.get_line() + 1
         charnum = iter_cursor.get_line_index()
@@ -27,27 +33,26 @@ class Jedi:
 
 class GediPlugin(GObject.Object, Gedit.ViewActivatable):
     __gtype_name__ = "GediPlugin"
-    py_extension = ".py"
     view = GObject.property(type=Gedit.View)
 
     def __init__(self):
         GObject.Object.__init__(self)
         self.completion_provider = None
+        self.conn_id = None
 
     def do_activate(self):
-        print("Gedi is activated.")
         document = self.view.get_buffer()
-        document.connect("loaded", self.on_document_load)
-
-        if document.get_uri_for_display().endswith(self.py_extension):
-            self.completion_provider = GediCompletionProvider()
-            self.view.get_completion().add_provider(self.completion_provider)
+        self.conn_id = document.connect("loaded", self.on_document_load)
 
     def do_deactivate(self):
-        print("Gedi is deactivated.")
+        document = self.view.get_buffer()
+        if self.conn_id:
+            document.disconnect(self.conn_id)
 
     def on_document_load(self, document, p3=None, p4=None, p5=0, p6=0):
-        if document.get_uri_for_display().endswith(self.py_extension):
+        hashbang_line = document.get_iter_at_line(
+            0).get_text(document.get_iter_at_line(1))
+        if document.props.mime_type == 'text/x-python' or (hashbang_line.startswith('#!/') and hashbang_line.find('python')):
             if self.completion_provider is None:
                 self.completion_provider = GediCompletionProvider()
                 self.view.get_completion().add_provider(self.completion_provider)
@@ -56,16 +61,6 @@ class GediPlugin(GObject.Object, Gedit.ViewActivatable):
                 self.view.get_completion().remove_provider(self.completion_provider)
                 self.completion_provider = None
 
-
-def get_icon_for_type(_type):
-    theme = Gtk.IconTheme.get_default()
-    try:
-        return theme.load_icon(icon_names[_type.lower()], 16, 0)
-    except:
-        try:
-            return theme.load_icon(Gtk.STOCK_ADD, 16, 0)
-        except:
-            return None
 
 class JediPopulator(Thread):
     def __init__(self, provider, context):
@@ -80,27 +75,31 @@ class JediPopulator(Thread):
         try:
             for completion in Jedi.get_script(self._document).completions():
                 complete = completion.name
-                if tuple(int(n) for n in jedi.__version__.split('.'))<= (0,7,0):
-                    doc=completion.doc
+                if tuple(int(n) for n in jedi.__version__.split('.')) <= (0, 7, 0):
+                    doc = completion.doc
                 else:
-                    doc=completion.docstring()
+                    doc = completion.docstring()
 
                 comp = GtkSource.CompletionItem.new()
                 comp.props.label = comp.props.text = completion.name
-                #comp.props.icon = get_icon_for_type(completion.type)
+                comp.props.icon_name = icon_names.get(
+                    completion.type.lower(), 'action-unavailable')
                 comp.props.info = doc
                 proposals.append(comp)
         except Exception:
             self.stop()
 
         if not self._stop_request.is_set():
-            GLib.idle_add(self._context.add_proposals, self._provider, proposals, True)
+            GLib.idle_add(self._context.add_proposals,
+                          self._provider, proposals, True)
 
     def stop(self):
-            self._stop_request.set()
+        self._stop_request.set()
+
     @property
     def stopped(self):
         return self._stop_request.is_set()
+
 
 class GediCompletionProvider(GObject.Object, GtkSource.CompletionProvider):
     __gtype_name__ = 'GediProvider'
@@ -108,19 +107,20 @@ class GediCompletionProvider(GObject.Object, GtkSource.CompletionProvider):
     def __init__(self):
         GObject.Object.__init__(self)
         self.thread = None
+
     def do_get_name(self):
         return _("Gedi Python Code Completion")
 
     def get_iter_correctly(self, context):
         if isinstance(context.get_iter(), tuple):
-            return context.get_iter()[1];
+            return context.get_iter()[1]
         else:
             return context.get_iter()
 
     def do_match(self, context):
         iter = self.get_iter_correctly(context)
         iter.backward_char()
-        buffer=iter.get_buffer()
+        buffer = iter.get_buffer()
         if buffer.get_context_classes_at_iter(iter) != ['no-spell-check']:
             return False
         ch = iter.get_char()
